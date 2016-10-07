55a0198d004fdbe44c535254e906c0aac3c84484
michaelfahmy <michaelfahmy1@gmail.com>
2016-10-07 21:26:34 +0200

    README.md updated

---
+++ README.md
@@ -0,0 +1,17 @@
+# Con Fusion
+Front-End JavaScript Frameworks: AngularJS course project.
+
+Under supervision of Hong Kong University of Science and Technology on [Coursera](www.coursera.com)
+
+[Verified certificate earned in August 2016] (https://www.coursera.org/account/accomplishments/certificate/PTRJFGENX9XG)
+
+## To get the project alive
+Navigate to the project directory and run the following commands
+
+`npm install`
+
+`bower install`
+
+`json-server --watch json-server/db.json` to run the local server
+
+`gulp`
---
+++ app/scripts/app.js
@@ -0,0 +1,93 @@
+'use strict';
+
+angular.module('confusionApp', ['ui.router', 'ngResource'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('app', {
+      url: '/',
+      views: {
+        'header': {
+          templateUrl: 'views/header.html'
+        },
+        'content': {
+          templateUrl: 'views/home.html',
+          controller: 'IndexController'
+        },
+        'footer': {
+          templateUrl: 'views/footer.html'
+        }
+      }
+    })
+
+    .state('app.aboutus', {
+      url: 'aboutus',
+      views: {
+        'content@': {
+          templateUrl: 'views/aboutus.html',
+          controller: 'AboutController'
+        }
+      }
+    })
+
+    .state('app.contactus', {
+      url: 'contactus',
+      views: {
+        'content@': {
+          templateUrl: 'views/contactus.html',
+          controller: 'ContactController'
+        }
+      }
+    })
+
+    .state('app.menu', {
+      url: 'menu',
+      views: {
+        'content@': {
+          templateUrl: 'views/menu.html',
+          controller: 'MenuController'
+        }
+      }
+    })
+
+    .state('app.dishdetail', {
+      url: 'menu/:id',
+      views: {
+        'content@': {
+          templateUrl: 'views/dishdetail.html',
+          controller: 'DishDetailController'
+        }
+      }
+    });
+
+  $urlRouterProvider.otherwise('/');
+
+});
+
+
+/*
+ * using ngRoute
+ */
+
+// angular.module('confusionApp', ['ngRoute'])
+// .config(function($routeProvider) {
+//   $routeProvider
+//     .when('/contactus', {
+//       templateUrl: 'contactus.html',
+//       controller: 'ContactController'
+//     })
+
+//     .when('/menu', {
+//       templateUrl: 'menu.html',
+//       controller: 'MenuController'
+//     })
+
+//     .when('/menu/:id', {
+//       templateUrl: 'dishdetail.html',
+//       controller: 'DishDetailController'
+//     })
+
+//     .otherwise('/contactus');
+
+// });
